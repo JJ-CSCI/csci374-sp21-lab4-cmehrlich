87727dda792eff4a00999fcfdb3116c09bd18271
cmehrlich <cmehrlich13@gmail.com>
2021-03-09 02:35:23 +0000

    recursive functions

--- main.fs
+++ main.fs
@@ -2,18 +2,23 @@ module Assignment
 
 // Problem 1
 let rec prod (lst:int list) =
+    if List.isEmpty lst
+    then 1
+    else List.head lst * prod (List.tail lst)
     // write your solution here
-    0
+    
 
 // Problem 2
 let rec map f (lst:int list) =
     // write your solution here
-    []
+   match lst with
+    [] -> []
+    h::t -> (f h)::(map f t)
 
 // Problem 3
 let rec odd (lst:int list) =
     // write your solution here
-    lst
+    
 
 // Problem 4
 let rec filter f lst =
